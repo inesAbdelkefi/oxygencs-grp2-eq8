71de88a9285e04f0561ee382f2056532a959e5b2
ines <ines.abdelkefi@mail.mcgill.ca>
2024-02-22 20:18:40 -0500

    Add env variables

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import logging
 import requests
 import json
 import time
-
+import os 
 
 class App:
     def __init__(self):
@@ -11,11 +11,11 @@ class App:
         self.TICKS = 10
 
         # To be configured by your team
-        self.HOST = None  # Setup your host here
-        self.TOKEN = None  # Setup your token here
-        self.T_MAX = None  # Setup your max temperature here
-        self.T_MIN = None  # Setup your min temperature here
-        self.DATABASE_URL = None  # Setup your database here
+        self.HOST = os.getenv("HOST")  # Utilize environment variable for HOST
+        self.TOKEN = os.getenv("TOKEN")  # Utilize environment variable for TOKEN
+        self.T_MAX = float(os.getenv("T_MAX"))  # Utilize environment variable for T_MAX
+        self.T_MIN = float(os.getenv("T_MIN"))  # Utilize environment variable for T_MIN
+        self.DATABASE_URL = os.getenv("DATABASE_URL")  # Utilize environment variable for DATABASE_URL
 
     def __del__(self):
         if self._hub_connection != None:
